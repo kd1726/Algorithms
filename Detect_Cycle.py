b5e42e06321a3386da28211f81bc5040dab9a926
kd1726 <kd1726@nyu.edu>
2021-04-09 17:43:24 -0400

    Unidrected Cycles

--- Detect_Cycle.py
+++ Detect_Cycle.py
@@ -5,21 +5,22 @@ class Graph:
         self.graph = defaultdict(list)
 
     def addEdge(self,index,vertex):
-        self.graph[index].append(vertex)
+     self.graph[index].append(vertex)
+     self.graph[vertex].append(index)
 
     def _detect_undirected_cycle(self,vertex,visited,parent):
         visited.add(vertex)
         for x in self.graph[vertex]:
             if x not in visited:
-                self._detect_undirected_cycle(self,x,visited,vertex)
-                return True
-            elif parent!=x:
+                self._detect_undirected_cycle(x,visited,vertex)
+                return False
+            elif parent==x:
                 return True
         return False
 
     def detect_undirected_cycle(self):
         visited = set()
         for x in list(self.graph):
-            if self._detect_undirected_cycle(self,x,visited,-1)==True:
+            if self._detect_undirected_cycle(x,visited,-1)==True:
                 return True
         return False
